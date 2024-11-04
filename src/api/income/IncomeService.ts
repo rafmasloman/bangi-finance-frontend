65df92ed6f76359a074b10d18c8968b6ca095074
rafmasloman <raflymasloman20@gmail.com>
2024-11-04 20:03:31 +0800

    feat: add console

--- src/api/income/IncomeService.ts
+++ src/api/income/IncomeService.ts
@@ -43,6 +43,8 @@ class IncomeService {
   async getIncomeAnalytics(
     historyId?: string,
   ): Promise<IApiBaseResponse<IIncomeAnalyticsResponseData>> {
+    console.log('history id : ', historyId);
+
     try {
       const response: AxiosResponse = await http.get(
         `${INCOME_API_ROUTE}/${historyId}/analytics`,
